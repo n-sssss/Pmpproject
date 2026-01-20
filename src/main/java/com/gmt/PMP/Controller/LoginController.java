23cd62d751bf525ffae52bba754fa8df72fa59cb
n-sssss <nsri68070@gmail.com>
2026-01-20 10:33:55 +0530

    hello2

--- src/main/java/com/gmt/PMP/Controller/LoginController.java
+++ src/main/java/com/gmt/PMP/Controller/LoginController.java
@@ -1,31 +1,26 @@
-package com.gmt.PMP;
+package com.gmt.PMP.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class HelloWorld {
+public class LoginController {
     @GetMapping("/login")
     public String showLoginPage() {
         return "Login"; // login.html
     }
-
+//    @PostMapping("/save")
+//    public String saveUser(Model m,@ModelAttribute User user) {
+//
+//    }
     @PostMapping("/login")
     public String login(
             @RequestParam String username,
             @RequestParam String password,
             Model model) {
-
-        // Dummy validation (for learning)
-        if (username.equals("admin") && password.equals("1234")) {
-            model.addAttribute("username", username);
-            return "login";
-        } else {
-            model.addAttribute("error", "Invalid username or password");
-            return "login";
-        }
+        return "Dashboard";
     }
+    
 }
